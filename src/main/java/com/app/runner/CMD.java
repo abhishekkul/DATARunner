2b1f35ea337f8ddbf3c5ac8d084acdbeef28c54b
lenovo <lenovo@DESKTOP-3EO33O0>
2019-06-07 13:03:58 +0530

    optionar and using

---
+++ src/main/java/com/app/runner/CMD.java
@@ -0,0 +1,16 @@
+package com.app.runner;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CMD    implements   CommandLineRunner {
+
+	public void run(String... args) throws Exception {
+
+		System.out.println(Arrays.asList(args));
+
+	} 
+}
